f22528798d7c9451826d8a52074f6e84b2edc759
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-11-25 18:06:52 +0300

    frist add commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+  alias(libs.plugins.androidApplication) apply false
+  alias(libs.plugins.kotlinAndroid) apply false
+  alias(libs.plugins.kotlinKsp) apply false
+  alias(libs.plugins.googleServices) apply false
+  alias(libs.plugins.hiltAndroid) apply false
+}
+true // Needed to make the Suppress annotation work for the plugins block
